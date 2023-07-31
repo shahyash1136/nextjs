fc7d2b002bbc0ccf3b4aae0bee899caa33c4865f
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2023-07-31 23:54:31 +0530

    completed till video 7 in section 5

---
+++ section-05/pages/index.js
@@ -0,0 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products?.map((el) => (
+        <li key={el.id}>{el.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(`${filePath}`);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
+
+export default HomePage;
