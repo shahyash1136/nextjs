c99ea07a29e81dcdb514cc80ae9143f183106a1f
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2023-08-22 23:29:42 +0530

    completed authentication and create store form

--- proj1/admin/app/(root)/page.tsx
+++ proj1/admin/app/(root)/page.tsx
@@ -1,10 +1,23 @@
-import { Button } from "@/components/ui/button";
+'use client';
 
-export default function Home() {
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export default function SetupPage() {
+    const onOpen = useStoreModal((state) => state.onOpen)
+    const isOpen = useStoreModal((state) => state.isOpen)
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen])
+
+
     return (
         <div className="p-4">
-            <p>Hello Admin Dashboard</p>
-            <Button size="default" variant="secondary">Click Me</Button>
+            Root Page
         </div>
     )
 }
