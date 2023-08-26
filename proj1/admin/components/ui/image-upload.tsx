58e83e7e913ad068cbdba6001a427522c73a0391
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2023-08-26 21:30:08 +0530

    completed billboard section

--- proj1/admin/components/ui/image-upload.tsx
+++ proj1/admin/components/ui/image-upload.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { Button } from "./button";
+import { CldUploadWidget } from 'next-cloudinary'
 
 interface ImageUploadProps {
     disabled?: boolean,
@@ -27,7 +31,40 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove,
 
     return (
         <div>
-            <div className="mb-4 flex items-center gap-4"></div>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map(url => {
+                    return (
+                        <div
+                            key={url}
+                            className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
+                            <div className="z-10 absolute top-2 right-2">
+                                <Button type="button" onClick={() => onRemove(url)} variant='destructive' size='icon'>
+                                    <Trash className="h-4 w-4" />
+                                </Button>
+                            </div>
+                            <Image
+                                fill
+                                className="object-cover"
+                                alt="Image"
+                                src={url}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+            <CldUploadWidget onUpload={onUpload} uploadPreset='deh3bi8p'>
+                {({ open }) => {
+                    const onClick = () => {
+                        open();
+                    }
+                    return (
+                        <Button type="button" disabled={disabled} variant='secondary' onClick={onClick}>
+                            <ImagePlus className="h-4 w-4 mr-2" />
+                            Upload Image
+                        </Button>
+                    )
+                }}
+            </CldUploadWidget>
         </div>
     )
 }
