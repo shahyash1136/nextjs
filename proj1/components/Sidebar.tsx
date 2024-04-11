ad088998e4457c6564d940001d9ab9340e525585
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2024-04-11 22:54:16 +0530

    completed the layout and started with the supabase setup

--- proj1/components/Sidebar.tsx
+++ proj1/components/Sidebar.tsx
@@ -4,6 +4,9 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SideBarProps {
     children: React.ReactNode;
@@ -32,9 +35,22 @@ const Sidebar: React.FC<SideBarProps> = ({ children }) => {
         <div className="flex h-full">
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
-                    Sidebar Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {
+                            routes.map((item) => (
+                                <SidebarItem key={item.label} {...item} />
+                            ))
+                        }
+
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library />
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     )
 }
