69874099378a4650f0d336c27fa8f2b5fe111a91
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2024-04-12 23:03:31 +0530

    completed the supabase setup

---
+++ proj1/providers/SupabaseProvider.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+    children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+    return (
+        <div>SupabaseProvider</div>
+    )
+}
+
+export default SupabaseProvider
+
